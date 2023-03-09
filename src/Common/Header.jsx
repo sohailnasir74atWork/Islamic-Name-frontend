6f4d95056a11c1ef08752d4b6b804aeb287706ba
sohailnasir74atWork <sohailnasir74business@gmail.com>
2023-03-09 21:31:33 +0500

    commmit

--- src/Common/Header.jsx
+++ src/Common/Header.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
-import "./header.css"
+import "./header.scss"
+import logo from "../images/logo.svg"
 const Header = () => {
   return (
     <div className='header-container'>
+      <div className='flex-box'>
+        <div className='logo-container'>
+          <img src={logo} alt="logo" /></div>
+        <div className='container'>
+          <ul>
+            <li>
+              99 Names of Allah
+            </li>
+            <li>
+              Tending Name
+            </li>
+            <li>
+              Boyes Names
+            </li>
+            <li>
+              Grils Name
+            </li>
+            <li>
+              Name from Quran
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
