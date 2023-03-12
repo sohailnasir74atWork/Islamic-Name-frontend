cca583d18dfc7993bf9f22759ed12e2a00f99c24
sohailnasir74atWork <sohailnasir74business@gmail.com>
2023-03-12 08:43:43 +0500

    commit

--- src/Screens/Home/TrendingSection.jsx
+++ src/Screens/Home/TrendingSection.jsx
@@ -16,21 +16,53 @@ const TrendingSection = () => {
         <h2 className='section-title-main'>SEARCH NAMES BY FEATURES</h2>
         <div className='section-container'>
           <div className='container w-30'>
-            <div className='col'>
-              <div className='col flex-center p-0 trending-card'>
+            <div className='col custimize-btn-cont'>
+              <div className='col flex-center p-0 w-100'>
                 <button
                   className={`custimize-btn ml-0 ${activeBtnIndex === 0 ? 'active' : ''}`}
                   onClick={() => handleButtonClick(0)}
                 >
-                  BOYES NAME
+                  Trending
                 </button>
               </div>
-              <div className='col flex-center p-0 trending-card'>
+              <div className='col flex-center p-0 w-100'>
                 <button
                   className={`custimize-btn  ${activeBtnIndex === 1 ? 'active' : ''}`}
                   onClick={() => handleButtonClick(1)}
                 >
-                  GIRLS NAME
+                  Popular
+                </button>
+              </div>
+              <div className='col flex-center p-0 w-100'>
+                <button
+                  className={`custimize-btn  ${activeBtnIndex === 2 ? 'active' : ''}`}
+                  onClick={() => handleButtonClick(2)}
+                >
+                  New Added
+                </button>
+              </div>
+              <div className='col flex-center p-0 w-100'>
+                <button
+                  className={`custimize-btn  ${activeBtnIndex === 3 ? 'active' : ''}`}
+                  onClick={() => handleButtonClick(3)}
+                >
+                  Qurnic Name
+                </button>
+              </div>
+              <div className='col flex-center p-0 w-100'>
+                <button
+                  className={`custimize-btn  ${activeBtnIndex === 4 ? 'active' : ''}`}
+                  onClick={() => handleButtonClick(4)}
+                >
+                  Allah's Name
+                </button>
+              </div>
+              <div className='col flex-center p-0 w-100'>
+                <button
+                  className={`custimize-btn  ${activeBtnIndex === 5 ? 'active' : ''}`}
+                  onClick={() => handleButtonClick(5)}
+                >
+                  Muhammad's Name
                 </button>
               </div>
             </div>
