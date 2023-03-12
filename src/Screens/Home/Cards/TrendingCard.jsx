6bd812a47096d453fb426e53b84ed46bd9c05bdb
sohailnasir74atWork <sohailnasir74business@gmail.com>
2023-03-12 22:58:41 +0500

    commit

--- src/Screens/Home/Cards/TrendingCard.jsx
+++ src/Screens/Home/Cards/TrendingCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "../Style/TrendingCard.scss"
 
 const Data = [
@@ -59,52 +59,73 @@ const Data = [
 ]
 
 const TrendingCard = () => {
-  const [like, setLike] = useState(0)
+  const [like, setLike] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedGender, setSelectedGender] = useState('GENDER');
+
   const handleLikeClick = () => {
-    setLike(like === 0 ? 1 : 0)
-  }
+    setLike(like === 0 ? 1 : 0);
+  };
+
+  const handleSelect = (gender) => {
+    setSelectedGender((prevGender) => {
+      console.log('prevGender:', prevGender);
+      console.log('gender:', gender);
+      return gender;
+    });
+    setIsOpen(false);
+  };
+  useEffect(() => {
+    console.log(selectedGender);
+  }, [selectedGender]);
+
   return (
     <div className='trending-card'>
       <div>
         <div className='d-flex'>
+          <th>NAMES</th>
           <th>
-            NAMES
+            <div className='gender-select'>
+              <div
+                className='gender-select-header'
+                onClick={() => setIsOpen(!isOpen)}
+                // onBlur={() => setIsOpen(false)}
+                tabIndex='0'
+              >
+                {selectedGender}
+                <i className='fa-solid fa-chevron-down p-2'></i>
+              </div>
+              {isOpen && (
+                <div className='gender-select-options'>
+                  <div onClick={() => handleSelect('BOYS')}>Boys</div>
+                  <div onClick={() => handleSelect('GIRLS')}>Girls</div>
+
+                </div>
+              )}
+            </div>
           </th>
-          <th>
-            GENDER
-          </th>
-          <th>
-            MEANINGS
-          </th>
-          <th className='urdu-cont'>
-            معنی
-          </th>
+          <th>MEANINGS</th>
+          <th className='urdu-cont'>معنی</th>
           <th className='support-cont'>
-            SUPPORT
-            <i class="fa-regular fa-heart fa-solid p-3 heart-w"></i>
+            SUPPORT <i class='fa-regular fa-heart fa-solid p-3 heart-w'></i>
           </th>
         </div>
-        {Data.map(item => {
-          return (<div className='d-flex line-color'>
-            <td>
-              {item.name}
-            </td>
-            <td>
-              {item.gender}
-            </td>
-            <td>
-              {item.meaning}
-            </td>
-            <td className='urdu-cont'>
-              {item.urdu}                   
-            </td><td className='urdu-cont'>
-              <span className='span-heart'>1650 People liked</span>
-              <i 
-              className={`fa-regular fa-heart${like === 1 ? ' fa-solid' : ''} p-3 heart`}
-              onClick={handleLikeClick}
-              ></i>
-            </td>
-          </div>)
+        {Data.map((item) => {
+          return (
+            <div className='d-flex line-color'>
+              <td>{item.name}</td>
+              <td>{item.gender}</td>
+              <td>{item.meaning}</td>
+              <td className='urdu-cont'>{item.urdu}</td>
+              <td className='urdu-cont'>
+                <span className='span-heart'>1650 People liked</span>
+                <i
+                  className={`fa-regular fa-heart${like === 1 ? ' fa-solid' : ''} p-3 heart`}
+                  onClick={handleLikeClick}
+                ></i>
+              </td>
+            </div>
+          );
         })}
         <div className='pagination-container'>
           <nav aria-label="Page navigation example">
